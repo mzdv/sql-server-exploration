35eacd2fd255c1838a2a706a94d73d4314511df1
Milos Zivadinovic <milos@ignispotentia.com>
2020-05-03 20:31:03 +0200

    Added view

--- create_tables.sql
+++ create_tables.sql
@@ -1,6 +1,6 @@
 USE [master]
 GO
-/****** Object:  Database [ddz_sql]    Script Date: 03-May-20 14:36:54 ******/
+/****** Object:  Database [ddz_sql]    Script Date: 03-May-20 20:30:02 ******/
 CREATE DATABASE [ddz_sql]
  CONTAINMENT = NONE
  ON  PRIMARY 
@@ -80,7 +80,7 @@ ALTER DATABASE [ddz_sql] SET QUERY_STORE = OFF
 GO
 USE [ddz_sql]
 GO
-/****** Object:  Table [dbo].[Company]    Script Date: 03-May-20 14:36:55 ******/
+/****** Object:  Table [dbo].[Company]    Script Date: 03-May-20 20:30:03 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -94,7 +94,7 @@ CREATE TABLE [dbo].[Company](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Customer]    Script Date: 03-May-20 14:36:55 ******/
+/****** Object:  Table [dbo].[Customer]    Script Date: 03-May-20 20:30:03 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -110,69 +110,7 @@ CREATE TABLE [dbo].[Customer](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Engine]    Script Date: 03-May-20 14:36:55 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Engine](
-	[Id] [bigint] NOT NULL,
-	[PistonOperated] [bit] NOT NULL,
- CONSTRAINT [PK_Engine] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[EnginesPistons]    Script Date: 03-May-20 14:36:55 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[EnginesPistons](
-	[EngineId] [bigint] NOT NULL,
-	[PistonId] [bigint] NOT NULL
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Machine]    Script Date: 03-May-20 14:36:55 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Machine](
-	[Id] [bigint] NOT NULL,
-	[NumberOfWidgets] [tinyint] NOT NULL,
- CONSTRAINT [PK_Machine] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[MachinesWidgets]    Script Date: 03-May-20 14:36:55 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[MachinesWidgets](
-	[MachineId] [bigint] NOT NULL,
-	[WidgetsId] [bigint] NOT NULL
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Piston]    Script Date: 03-May-20 14:36:55 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Piston](
-	[Id] [bigint] IDENTITY(1,1) NOT NULL,
-	[Name] [nvarchar](max) NOT NULL,
- CONSTRAINT [PK_Piston] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Product]    Script Date: 03-May-20 14:36:55 ******/
+/****** Object:  Table [dbo].[Product]    Script Date: 03-May-20 20:30:03 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -188,7 +126,81 @@ CREATE TABLE [dbo].[Product](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Widget]    Script Date: 03-May-20 14:36:55 ******/
+/****** Object:  View [dbo].[CRM]    Script Date: 03-May-20 20:30:03 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE VIEW [dbo].[CRM]
+AS
+SELECT        dbo.Customer.Name, dbo.Customer.Address, dbo.Product.Name AS [Product name], dbo.Company.Name AS Company
+FROM            dbo.Company INNER JOIN
+                         dbo.Customer ON dbo.Company.Id = dbo.Customer.CompanyId INNER JOIN
+                         dbo.Product ON dbo.Company.Id = dbo.Product.CompanyId
+GO
+/****** Object:  Table [dbo].[Engine]    Script Date: 03-May-20 20:30:03 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Engine](
+	[Id] [bigint] NOT NULL,
+	[PistonOperated] [bit] NOT NULL,
+ CONSTRAINT [PK_Engine] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[EnginesPistons]    Script Date: 03-May-20 20:30:03 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[EnginesPistons](
+	[EngineId] [bigint] NOT NULL,
+	[PistonId] [bigint] NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Machine]    Script Date: 03-May-20 20:30:03 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Machine](
+	[Id] [bigint] NOT NULL,
+	[NumberOfWidgets] [tinyint] NOT NULL,
+ CONSTRAINT [PK_Machine] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[MachinesWidgets]    Script Date: 03-May-20 20:30:03 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[MachinesWidgets](
+	[MachineId] [bigint] NOT NULL,
+	[WidgetsId] [bigint] NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Piston]    Script Date: 03-May-20 20:30:03 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Piston](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK_Piston] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Widget]    Script Date: 03-May-20 20:30:03 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -219,6 +231,8 @@ ALTER TABLE [dbo].[EnginesPistons] CHECK CONSTRAINT [FK_EnginesPistons_Engine]
 GO
 ALTER TABLE [dbo].[EnginesPistons]  WITH CHECK ADD  CONSTRAINT [FK_EnginesPistons_Piston] FOREIGN KEY([PistonId])
 REFERENCES [dbo].[Piston] ([Id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
 GO
 ALTER TABLE [dbo].[EnginesPistons] CHECK CONSTRAINT [FK_EnginesPistons_Piston]
 GO
@@ -234,6 +248,8 @@ ALTER TABLE [dbo].[MachinesWidgets] CHECK CONSTRAINT [FK_MachinesWidgets_Machine
 GO
 ALTER TABLE [dbo].[MachinesWidgets]  WITH CHECK ADD  CONSTRAINT [FK_MachinesWidgets_Widget] FOREIGN KEY([WidgetsId])
 REFERENCES [dbo].[Widget] ([Id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
 GO
 ALTER TABLE [dbo].[MachinesWidgets] CHECK CONSTRAINT [FK_MachinesWidgets_Widget]
 GO
@@ -241,6 +257,137 @@ ALTER TABLE [dbo].[Product]  WITH CHECK ADD  CONSTRAINT [FK_Product_Company] FOR
 REFERENCES [dbo].[Company] ([Id])
 GO
 ALTER TABLE [dbo].[Product] CHECK CONSTRAINT [FK_Product_Company]
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "Company"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 102
+               Right = 208
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "Customer"
+            Begin Extent = 
+               Top = 162
+               Left = 399
+               Bottom = 292
+               Right = 569
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "Product"
+            Begin Extent = 
+               Top = 9
+               Left = 422
+               Bottom = 139
+               Right = 592
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 1440
+         Alias = 2430
+         Table = 1170
+         Output = 1695
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'CRM'
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=1 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'CRM'
 GO
 USE [master]
 GO
